79a389055d79096decf6ec707a4f94300ffa1339
Ilya Solozobov <i.solozobov@gmail.com>
2020-08-06 19:20:08 +0300

    Save square are instead of rect
    add file with mouth rectangles

--- gen/sample/dlibPoints.py
+++ gen/sample/dlibPoints.py
@@ -24,6 +24,23 @@ if not os.path.isfile(dlib_predictor):
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(dlib_predictor)
 
+def getSquare(rect):
+	height = rect[3] - rect[1]
+	width = rect[2] - rect[0]
+
+	maxSide = max(height, width)
+
+	centerX = (rect[0] + rect[2]) / 2
+	centerY = (rect[1] + rect[3]) / 2
+
+	newRect = (int(centerX - maxSide / 2),
+			   int(centerY - maxSide / 2),
+			   int(centerX + maxSide / 2),
+			   int(centerY + maxSide / 2))
+
+	return newRect
+
+
 def getRect(vecX, vecY):
 	tx = min(vecX)
 	ty = min(vecY)
@@ -39,22 +56,22 @@ def saveMouthRect(shape, fileName):
 		vecX.append(shape.part(b).x)
 		vecY.append(shape.part(b).y)
 
-	rect = getRect(vecX, vecY)
+	square = getSquare(getRect(vecX, vecY))
 
 	writeString = fileName + " " \
-				  		   + str(rect[0]) + " " \
-						   + str(rect[1]) + " "\
-						   + str(rect[2]) + " "\
-						   + str(rect[3]) + "\n"
+				  		   + str(square[0]) + " " \
+						   + str(square[1]) + " "\
+						   + str(square[2]) + " "\
+						   + str(square[3]) + "\n"
 
 	mouthFile.write(writeString)
 
 onlyfiles = [f for f in listdir(args.src) if isfile(join(args.src, f)) and f.endswith(".jpg")]
 
-mouthFile = open("mouthDlibMeta.txt", 'w')
+mouthFile = open(args.src +"/mouthDlibMeta.txt", 'w')
 
 for i in range(0, len(onlyfiles)):
-	print("Process ", str(i) + "/" + str(len(onlyfiles)))
+	print("Process ", str(i+1) + "/" + str(len(onlyfiles)))
 
 	file = onlyfiles[i]
 	img = cv2.imread(args.src + "/" + file)
